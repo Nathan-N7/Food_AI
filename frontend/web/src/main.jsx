f8c5cecf0a24181b0949593816081f4807356246
Nathan-N7 <nathan.rod1710@gmail.com>
2026-08-30 16:00:30 -0300

    PWA pronto!

--- frontend/web/src/main.jsx
+++ frontend/web/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { registerSW } from 'virtual:pwa-register'
 
 import './index.css'
 import App from './App.jsx'
 
+const updateSW = registerSW({
+  onNeedRefresh() {
+  },
+  onOfflineReady() {
+    console.log('O app está pronto para funcionar offline!')
+  },
+})
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
